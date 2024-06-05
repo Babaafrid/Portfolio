37095125364ae458066ef05b073a9f88b111ed88
Baba Afrid <babaafrid7@gmail.com>
2024-06-05 14:52:14 +0530

    Updates made

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,74 +3,74 @@ import "./Skills.css";
 
 function Skills() {
   return (
-    <div class="skill-container">
+    <div className="skill-container">
       <h2 className="skill-heading">My <span className="lime">Skills</span></h2>
-      <div class="skill-box">
-        <span class="skill-title">Java</span>
+      <div className="skill-box">
+        <span className="skill-title">Java</span>
 
-        <div class="skill-bar">
-          <span class="skill-per java">
-            <span class="tooltip">85%</span>
+        <div className="skill-bar">
+          <span className="skill-per java">
+            <span className="tooltip">85%</span>
           </span>
         </div>
       </div>
 
-      <div class="skill-box">
-        <span class="skill-title">Python</span>
+      <div className="skill-box">
+        <span className="skill-title">Python</span>
 
-        <div class="skill-bar">
-          <span class="skill-per python">
-            <span class="tooltip">75%</span>
+        <div className="skill-bar">
+          <span className="skill-per python">
+            <span className="tooltip">75%</span>
           </span>
         </div>
       </div>
 
-      <div class="skill-box">
-        <span class="skill-title">Java Script</span>
+      <div className="skill-box">
+        <span className="skill-title">Java Script</span>
 
-        <div class="skill-bar">
-          <span class="skill-per javascript">
-            <span class="tooltip">80%</span>
+        <div className="skill-bar">
+          <span className="skill-per javascript">
+            <span className="tooltip">80%</span>
           </span>
         </div>
       </div>
 
-      <div class="skill-box">
-        <span class="skill-title">ReactJs</span>
+      <div className="skill-box">
+        <span className="skill-title">ReactJs</span>
 
-        <div class="skill-bar">
-          <span class="skill-per react">
-            <span class="tooltip">90%</span>
+        <div className="skill-bar">
+          <span className="skill-per react">
+            <span className="tooltip">90%</span>
           </span>
         </div>
       </div>
 
-      <div class="skill-box">
-        <span class="skill-title">NodeJs</span>
+      <div className="skill-box">
+        <span className="skill-title">NodeJs</span>
 
-        <div class="skill-bar">
-          <span class="skill-per nodejs">
-            <span class="tooltip">80%</span>
+        <div className="skill-bar">
+          <span className="skill-per nodejs">
+            <span className="tooltip">80%</span>
           </span>
         </div>
       </div>
 
-      <div class="skill-box">
-        <span class="skill-title">Django</span>
+      <div className="skill-box">
+        <span className="skill-title">Django</span>
 
-        <div class="skill-bar">
-          <span class="skill-per django">
-            <span class="tooltip">70%</span>
+        <div className="skill-bar">
+          <span className="skill-per django">
+            <span className="tooltip">70%</span>
           </span>
         </div>
       </div>
 
-      <div class="skill-box">
-        <span class="skill-title">MySQL</span>
+      <div className="skill-box">
+        <span className="skill-title">MySQL</span>
 
-        <div class="skill-bar">
-          <span class="skill-per mysql">
-            <span class="tooltip">75%</span>
+        <div className="skill-bar">
+          <span className="skill-per mysql">
+            <span className="tooltip">75%</span>
           </span>
         </div>
       </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,6 +8,7 @@ import hashinclude from "../assets/hashinclude.png"
 import focus from "../assets/focus.png"
 import carrental from "../assets/car-rental.png"
 import hli from "../assets/hli.png"
+import movie from "../assets/movie.png";
 import google from "../assets/google.png"
 import bg from "../assets/bg.png"
 import qr from "../assets/qr.png"
@@ -16,6 +17,7 @@ import snake from "../assets/snake.png"
 import tictactoe from "../assets/tictactoe.png"
 import spotify from "../assets/spotify.png"
 import scm from "../assets/scm.jpg"
+import sort from "../assets/sort.png";
 
 function Projects() {
   return (
@@ -27,7 +29,9 @@ function Projects() {
         <ProjectCard img={focus} name="FOCUS" link="https://klfocus.netlify.app/" />
         <ProjectCard img={carrental} name="Car Rental System" git="https://github.com/Babaafrid/Car_Rental" />
         <ProjectCard img={hli} name="Health and Life Insurance System" git="https://github.com/Babaafrid/HLI" />
+        <ProjectCard img={movie} name="Movie Library Web Application" git="https://github.com/Babaafrid/movie-library-frontend/" link="https://dev-movie-library.netlify.app/" />
         <ProjectCard img={scm} name="Supply Chain Management" git="https://github.com/Babaafrid/Supply-Chain-Management" />
+        <ProjectCard img={sort} name="Sorting Visualizer" git="https://github.com/Babaafrid/sorting-visualizer" link="https://dev-sorting-visualizer.netlify.app/" />
         <ProjectCard img={google} name="Google Clone" git="https://github.com/Babaafrid/google_clone" link="https://googleclone7.netlify.app/" />
         <ProjectCard img={bg} name="Background Image Remover" git="https://github.com/Babaafrid/bg-remover" link="https://afrid-bgremover.netlify.app/" />
         <ProjectCard img={qr} name="QR Code Generator" git="https://github.com/Babaafrid/qrcode" link="https://afrid-qrcode.netlify.app/" />
